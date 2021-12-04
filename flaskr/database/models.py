fd83a83814f28fb1c94160a71822d328f2a50e28
omar ashraf <omarashraf465@yahoo.com>
2021-12-04 15:03:40 +0200

    updating db url

--- flaskr/database/models.py
+++ flaskr/database/models.py
@@ -8,8 +8,8 @@ db = SQLAlchemy()
 
 
 def setupDB(app,database_path=database_path):
-    #app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:Email1087@localhost:5432/agency'
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    #app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:Email1087@localhost:5432/agency'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
