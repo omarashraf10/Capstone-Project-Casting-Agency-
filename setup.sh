26f4da44357f11fe265fa87842050ae38f1bc21b
omar ashraf <omarashraf465@yahoo.com>
2021-12-04 15:59:26 +0200

    edit os env

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ export API_AUDIENCE="Agency"
 export ALGORITHMS=['RS256']
 export FLASK_APP=flaskr
 export FLASK_ENV=development
-export DATABASE_URL="postgres://ylaxdrdccwkyfc:da1f14f338d94932f33905dedd4ce95aad945755897d2cdc4a2755e0c4ce468c@ec2-18-208-102-44.compute-1.amazonaws.com:5432/d68n857frnfinv"
+export DATABASE_URL="postgresql://ylaxdrdccwkyfc:da1f14f338d94932f33905dedd4ce95aad945755897d2cdc4a2755e0c4ce468c@ec2-18-208-102-44.compute-1.amazonaws.com:5432/d68n857frnfinv"
